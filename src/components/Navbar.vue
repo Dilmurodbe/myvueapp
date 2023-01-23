72a1fd0740c80a5be501e392ab9117c27bdc60a5
dilmurodbe <saydullayevdilmurod233@gmail.com>
2023-01-23 15:29:28 +0500

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'NavbarPage'
+}
+import "../assets/main.css"
+</script>
+<template>
+
+<h1 class="heading" style="color: aqua;">this is navbar</h1>
+<div v-for="item in 10">
+  <li> {{ item }}</li>
+</div>
+</template>
+
+<style>
+
+
+</style>
